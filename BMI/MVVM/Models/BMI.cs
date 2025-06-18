086a03ed41e28d2cc79c42205f03bf0ce834a9af
Wolvye <alainsoltau@googlemail.com>
2025-06-18 10:12:56 +0200

    Update

---
+++ BMI/MVVM/Models/BMI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI.MVVM.Models
+{
+    public class BMI
+    {
+        private float result;
+
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public float Result 
+        
+        {   get
+            {
+                return ((Weight / Height) / Height) * 10000;
+            }
+        }
+    }
+}
